a036d45a5ec9713d0187f5fbc5f1bf2b4ce5e906
repolevedavaj <jonas.bamberger@gmail.com>
2022-05-11 09:49:44 +0200

    switched to new splash-screen lib

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider, IconRegistry, Layout} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {NavigationContainer} from "@react-navigation/native";
 import GrowBuddyDatabaseService from "./src/services/database/GrowBuddyDatabaseService";
-import AppLoading from "expo-app-loading";
 import DefaultStack from "./src/stacks/default/DefaultStack";
 import {Appearance, SafeAreaView, StatusBar} from "react-native";
 import * as Sentry from 'sentry-expo';
+import * as SplashScreen from 'expo-splash-screen';
 
 Sentry.init({
     dsn: 'https://87b522a85d234062b39f044f423848af@o566027.ingest.sentry.io/5708282',
@@ -27,18 +27,24 @@ const App = () => {
 
     const [initialized, setInitialized] = useState(false);
 
-    const initialize = async () => {
-        await GrowBuddyDatabaseService.openDatabase();
-    }
+    useEffect(() => {
+        async function initialize() {
+            await SplashScreen.preventAutoHideAsync();
+            await GrowBuddyDatabaseService.openDatabase();
+            setInitialized(true);
+        }
+
+        initialize();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (initialized) {
+            await SplashScreen.hideAsync();
+        }
+    }, [initialized]);
 
     if (!initialized) {
-        return (
-            <AppLoading
-                startAsync={initialize}
-                onFinish={() => setInitialized(true)}
-                onError={(error) => console.log(error)}
-            />
-        );
+        return null;
     }
 
     return (
@@ -46,7 +52,7 @@ const App = () => {
             <StatusBar barStyle={themeStatusBarStyle}/>
             <IconRegistry icons={EvaIconsPack}/>
             <ApplicationProvider {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}>
-                <Layout style={{flex: 1}}>
+                <Layout style={{flex: 1}} onLayout={onLayoutRootView}>
                     <SafeAreaView style={{flex: 1}}>
                         <DefaultStack/>
                     </SafeAreaView>
